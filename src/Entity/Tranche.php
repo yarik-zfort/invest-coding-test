e881c5d4c1ec454c3d4b3aed2e4f9564a0d87bcf
yaroslav.pustovyi <yaroslav.pustovyi@zfort.com>
2019-08-14 09:05:20 +0300

    add phpdocs and some fix

--- src/Entity/Tranche.php
+++ src/Entity/Tranche.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+
 namespace LendInvest\Entity;
 
 
@@ -11,16 +12,39 @@ class Tranche
      */
     private $loan;
 
+    /**
+     * @var float
+     */
     private $maxAmount;
 
+    /**
+     * @var float
+     */
     private $monthlyRate;
 
+    /**
+     * @var int
+     */
     private $totalAmount = 0;
 
+    /**
+     * @var string
+     */
     private $name;
 
+    /**
+     * @var \SplObjectStorage
+     */
     private $transactions;
 
+    /**
+     * Tranche constructor.
+     * @param Loan $loan
+     * @param float $maxAmount
+     * @param float $monthlyRate
+     * @param string $name
+     * @throws \Exception
+     */
     public function __construct(
         Loan $loan,
         float $maxAmount,
@@ -37,8 +61,15 @@ class Tranche
         $this->transactions = new \SplObjectStorage();
         $this->loan = $loan;
         $this->loan->addTranche($this);
+        $this->name = $name;
     }
 
+    /**
+     * @param float $amount
+     * @param Investor $investor
+     * @return Transaction
+     * @throws \Exception
+     */
     public function investmentProcessing(float $amount, Investor $investor): Transaction
     {
         if (!$this->loan->isOpen($this->getCurrentDate())) {
@@ -62,6 +93,11 @@ class Tranche
         return $transaction;
     }
 
+    /**
+     * @param float $maxAmount
+     * @return bool
+     * @throws \Exception
+     */
     private function isMaxAmountValid(float $maxAmount): bool
     {
         if ($maxAmount < 0) {
@@ -70,6 +106,11 @@ class Tranche
         return true;
     }
 
+    /**
+     * @param float $monthlyRate
+     * @return bool
+     * @throws \Exception
+     */
     private function isMonthlyRateValid(float $monthlyRate): bool
     {
         if ($monthlyRate < 0) {
@@ -103,6 +144,14 @@ class Tranche
     }
 
     /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
      * @return \SplObjectStorage
      */
     public function getTransactions(): \SplObjectStorage
@@ -110,6 +159,10 @@ class Tranche
         return $this->transactions;
     }
 
+    /**
+     * @return \DateTimeImmutable
+     * @throws \Exception
+     */
     public function getCurrentDate(): \DateTimeImmutable
     {
         return new \DateTimeImmutable();
